869a91245ece1193fda95f32683c7060a59bce3c
Graham Wiese <graham.wiese@gmail.com>
2021-09-02 11:10:36 +0930

    Grinberg version

--- config.py
+++ config.py
@@ -17,8 +17,8 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['your-email@example.com']
-    LANGUAGES = ['en', 'es']
+    ADMINS = ['gwiese@tpg.com.au']
+    LANGUAGES = ['en', 'es', 'de']
     MS_TRANSLATOR_KEY = os.environ.get('MS_TRANSLATOR_KEY')
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
     POSTS_PER_PAGE = 25
